cb7986df37bf48ce7bc6f82261a6c0593c65f02b
Zenkin Fukuzawa <zennkinnn@MacBook-Air.local>
2026-05-05 15:35:29 +0900

    Configure Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const site = process.env.SITE_URL ?? 'https://replace-me.example.com';
+const site = process.env.SITE_URL ?? process.env.URL ?? 'https://replace-me.example.com';
 
 export default defineConfig({
   site,
